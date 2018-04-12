be5d1641c5da99902ea21c6c542a903005fe0e89
q2012 <vlad9696@ukr.net>
2018-04-12 15:41:37 +0300

    EspServer_Initial2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     app     = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var opened = 0,
     init = 0,
